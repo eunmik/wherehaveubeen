da466ceacc8637f927fd6e3a896d38934be0a92a
eunmik <eunmik1022@gmail.com>
2021-12-23 18:00:01 +0900

    social login page, server - ing

---
+++ login-fe/login.js
@@ -0,0 +1,16 @@
+let x = document.getElementById('login');
+let y = document.getElementById('register');
+let z = document.getElementById('btn');
+
+function register(){
+  x.style.left = '-600px';
+  y.style.left = '10px';
+  z.style.left = '110px';
+}
+
+function login() {
+  x.style.left = '10px';
+  y.style.left = '400px';
+  z.style.left = '0px';
+
+}
