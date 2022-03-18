ba6226e331beca635e2436f58649430f11733e67
eunmik <eunmik1022@gmail.com>
2022-03-18 18:02:10 +0900

    [Login] Kakao 로그인 API 추가

--- spring-social/src/main/java/com/example/springsocial/security/oauth2/user/OAuth2UserInfoFactory.java
+++ spring-social/src/main/java/com/example/springsocial/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -16,6 +16,8 @@ public class OAuth2UserInfoFactory {
             return new GithubOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.naver.toString())) {
             return new NaverOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.kakao.toString())) {
+            return new KakaoOAuth2UserInfo(attributes);
         }
 
         else {
