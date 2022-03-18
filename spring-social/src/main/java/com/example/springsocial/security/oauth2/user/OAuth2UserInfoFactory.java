fe4ae5e866641bdf00a1cf68294211005a81f15e
eunmik <eunmik1022@gmail.com>
2022-03-18 16:46:15 +0900

    [Login] Naver 로그인 API 연동을 위한 코드 추가
    네이버 로그인 연동

--- spring-social/src/main/java/com/example/springsocial/security/oauth2/user/OAuth2UserInfoFactory.java
+++ spring-social/src/main/java/com/example/springsocial/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -14,7 +14,11 @@ public class OAuth2UserInfoFactory {
             return new FacebookOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
             return new GithubOAuth2UserInfo(attributes);
-        } else {
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.naver.toString())) {
+            return new NaverOAuth2UserInfo(attributes);
+        }
+
+        else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
         }
     }
